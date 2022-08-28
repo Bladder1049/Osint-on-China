89903f044817fec2608cca6aac83b3063dc59157
Shaun Chander <shaunchander@gmail.com>
2022-08-28 14:02:02 -0500

    add netlify cms

---
+++ src/components/articles/ArticleSlice.svelte
@@ -0,0 +1,18 @@
+<script>
+	// Component imports
+	import { Hero, Container, Text } from "@utils"
+</script>
+
+<Hero>
+	<Container>
+		<div>
+			<!-- Heading -->
+			<div>
+				<Text style="snall">April 17, 2022 • 3 Minutes Read</Text>
+				<Text style="h2" tag="h1" className="text-orange-500"
+					>How to make a composited typing</Text
+				>
+			</div>
+		</div>
+	</Container>
+</Hero>
