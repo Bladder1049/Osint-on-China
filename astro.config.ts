08075554bdabaec31a67751f590346fc4cf2ed8e
Bladder1049 <109539231+Bladder1049@users.noreply.github.com>
2024-12-23 13:19:35 +0800

    Add files via upload

--- astro.config.ts
+++ astro.config.ts
@@ -9,38 +9,41 @@ import { VitePWA } from "vite-plugin-pwa"
 import { manifest, defaultSEO } from "./helpers/seoConfig"
 
 export default defineConfig({
-	site: defaultSEO.baseURL,
-	integrations: [
-		tailwind({
-			config: {
-				applyBaseStyles: false
-			}
-		}),
-		svelte(),
-		sitemap(),
-		compress()
-	],
-	vite: {
-		plugins: [
-			SvelteSVG({
-				svgoConfig: {
-					multipass: true
-				}
-			}),
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-					globDirectory: "dist",
-					globPatterns: [
-						"**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,html}"
-					],
-					navigateFallback: null
-				}
-			})
-		],
-		build: {
-			cssCodeSplit: false
-		}
-	}
+  site: defaultSEO.baseURL,
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false
+      }
+    }),
+    svelte(),
+    sitemap(),
+    compress()
+  ],
+  vite: {
+    plugins: [
+      SvelteSVG({
+        svgoConfig: {
+          multipass: true
+        }
+      }),
+      VitePWA({
+        registerType: "autoUpdate",
+        manifest,
+        workbox: {
+          globDirectory: "dist",
+          globPatterns: [
+            "**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico,html}"
+          ],
+          navigateFallback: null
+        }
+      })
+    ],
+    build: {
+      cssCodeSplit: false
+    },
+    ssr: {
+      noExternal: ["svelte-markdown"]
+    }
+  }
 })
